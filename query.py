27ff1b6f42f242411ba4a9fa7f658c0e9710e958
Steffen Ronalter <ronalterde@gmail.com>
2018-01-25 21:44:56 +0100

    Add usage for wrong number of input args

--- query.py
+++ query.py
@@ -9,24 +9,27 @@ import time
 from fs_dir import *
 import config
 
-TARGET_OWNER = sys.argv[1]
-
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('usage: %s <target user>' % sys.argv[0])
+        exit(1)
+
+    target_user = sys.argv[1]
     conf = config.load()
     base_dir = conf['base_dir']
     me = conf['me']
 
     fs_session_directory = FsSessionDirectory(base_dir)
     sessions = fs_session_directory.list_sessions()
-    matching_sessions = [x for x in sessions if x['owner'] == TARGET_OWNER]
+    matching_sessions = [x for x in sessions if x['owner'] == target_user]
 
     if len(matching_sessions) == 1:
-        print('Session for user %s running. Scheduled end time: %s' % (TARGET_OWNER, matching_sessions[0]['end_time']))
+        print('Session for user %s running. Scheduled end time: %s' % (target_user, matching_sessions[0]['end_time']))
         print('Filing request...')
         modified_session = dict(sessions[0])
         modified_session = append_request_to_session(modified_session, me)
         fs_session_directory.update_session(modified_session)
     else:
-        print('No session for user %s running.' % TARGET_OWNER)
+        print('No session for user %s running.' % target_user)
 
 
