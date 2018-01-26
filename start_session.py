cd74aaad49fefccd98e5a3c980b283d146865f57
Steffen Ronalter <ronalterde@gmail.com>
2018-01-26 06:40:48 +0100

    Refactor start_session

--- start_session.py
+++ start_session.py
@@ -9,6 +9,12 @@ from fs_dir import *
 import config
 import tkgui
 
+class Timer:
+    def start(self, end_time):
+        while datetime.now() < end_time:
+            print('Seconds left: ' + str((end_time - datetime.now()).total_seconds()) + '\r', sep=' ', end='', flush=True)
+            time.sleep(1)
+
 if __name__ == "__main__":
     conf = config.load()
     base_dir = conf['base_dir']
@@ -19,15 +25,12 @@ if __name__ == "__main__":
     fs_session_directory = FsSessionDirectory(base_dir)
     end_time = datetime.now() + time_box
     fs_session_directory.add_session({'owner' : me, 'end_time' : end_time})
-    sessions = fs_session_directory.list_sessions()
 
-    print('Timer (%s) started. End time: %s' % (time_box, end_time))
-
-    while datetime.now() < end_time:
-        print('Seconds left: ' + str((end_time - datetime.now()).total_seconds()) + '\r', sep=' ', end='', flush=True)
-        time.sleep(1)
-
+    timer = Timer()
+    print('Starting timer (%s). End time: %s' % (time_box, end_time))
+    timer.start(end_time)
     print('Session done.')
+
     sessions = fs_session_directory.list_sessions()
     my_sessions = [x for x in sessions if x['owner'] == me]
 
