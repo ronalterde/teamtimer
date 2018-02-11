b374e4c5e09f26a845f126f1948ae6bebda1aac5
Steffen Ronalter <ronalterde@gmail.com>
2018-02-11 10:28:53 +0100

    Remove class Session

--- session.py
+++ session.py
@@ -49,25 +49,3 @@ class SessionManager:
 
     def _calculate_end_time(self, duration):
         return self.time_provider.get_current_time() + duration
-
-class Session:
-    def __init__(self, session_storage, time_provider, username):
-        self.session_storage = session_storage
-        self.time_provider = time_provider
-        self.username = username
-    
-    def start(self, duration):
-        if duration.total_seconds() == 0:
-            return
-        if self.username == '':
-            return
-
-        self.session_storage.remove_sessions_owned_by(self.username)
-        self.session_storage.add_session({'end_time' : self.end_time(duration), 'owner' : self.username })
-        return self.end_time(duration)
-
-    def stop(self):
-        return []
-
-    def end_time(self, duration):
-        return self.time_provider.get_current_time() + duration
